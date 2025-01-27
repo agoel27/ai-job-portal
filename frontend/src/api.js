72ed4bb62576647cd061833cadd8c2297a1878ac
agoel27 <aryangoel27@gmail.com>
2025-01-27 07:55:43 -0800

    Initial commit

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+// Intercepter automatically adds header using axios
+// Everytime we send a request axios intercepter will intercept the request to see 
+// if the request has the token. If the request doesn't, axios will add the access token
+
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api
