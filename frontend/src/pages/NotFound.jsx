f57d74c75082597cb10ad62194c473c45d598db3
Kevin Loritsch <kloritsch58@gmail.com>
2025-03-07 10:48:09 -0800

    working w/out conflicts

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,8 @@
 function NotFound() {
-    return <div>
-        <h1>404 Not Found</h1>
-        <p className="text-amber-300">The page you are looking for doesn&apos;t exist!</p>
-    </div>
-  );
+  return <div>
+      <h1>404 Not Found</h1>
+      <p className="text-amber-300">The page you are looking for doesn&apos;t exist!</p>
+  </div>
 }
 
-export default NotFound;
+export default NotFound
