fae47bc47f33c8bc9649092effccd02f72620809
Osanna Deng <143480560+osannadeng@users.noreply.github.com>
2025-03-10 08:38:11 -0700

    migrate to tailwind

--- frontend/src/pages/SubmitResume.jsx
+++ frontend/src/pages/SubmitResume.jsx
@@ -1,10 +1,8 @@
-import "../styles/SubmitResume.css";
-
 function SubmitResume() {
     return (
-        <div>
-            <h1 className="header-text">Submit Resume</h1>
-            <div className="bottom-border"></div>
+        <div className="group absolute w-full top-3">
+            <h1 className="text-[40px] !important ml-[20%] font-sans font-bold text-[#382D5E] mb-[10px]">Submit Resume</h1>
+            <div className="w-[60%] mx-auto border-b border-black"></div>
         </div>
     )
 }
