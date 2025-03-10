9535adf70deee803fba98749db3034117f3533ce
Aryan Goel <aryangoel27@gmail.com>
2025-03-10 09:57:47 -0700

    changed not found text color

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ function NotFound() {
   return (
     <div>
       <h1>404 Not Found</h1>
-      <p className="text-amber-300">
+      <p className="text-green-800">
         The page you are looking for doesn&apos;t exist!
       </p>
     </div>
