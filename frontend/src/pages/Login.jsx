c9043090998c777f1dc1356420ab628fa35534c7
Arielle Haryanto <arielleharyanto@gmail.com>
2025-03-10 09:54:37 -0700

    merge conflicts

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,14 @@
-import Form from "../components/Form"
+import Form from "../components/Form";
+import AILogo from "../../public/AILogo.webp";
+import "../styles/Form.css";
 
 function Register() {
-    return <Form route="/api/token/" method="login" />
+  return (
+    <div className="container">
+      <img src={AILogo} alt="AI Logo" className="logo" />
+      <Form route="/api/token/" method="login" />
+    </div>
+  );
 }
 
-export default Register
+export default Register;
