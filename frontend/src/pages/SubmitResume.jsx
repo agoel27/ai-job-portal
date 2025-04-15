3226febf065bd4963ede163750835e918bb99e93
Osanna Deng <143480560+osannadeng@users.noreply.github.com>
2025-04-14 17:26:44 -0700

    reorganize submit resume, shift to standard tailwind values

--- frontend/src/pages/SubmitResume.jsx
+++ frontend/src/pages/SubmitResume.jsx
@@ -1,10 +1,9 @@
+import Title from "../components/submit_resume/Title";
+
 function SubmitResume() {
   return (
-    <div className="group absolute w-full top-3">
-      <h1 className="text-[40px] !important ml-[20%] font-sans font-bold text-[#382D5E] mb-[10px]">
-        Submit Resume
-      </h1>
-      <div className="w-[60%] mx-auto border-b border-black"></div>
+    <div>
+      <Title>Submit Resume</Title>
     </div>
   );
 }
